c3d8c4772a8a445bdda173dd428b29ed30305f82
aminetiouk <tiouk.amine@gmail.com>
2025-02-24 22:30:53 +0000

    feat: add feedback item rendering function

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
 const submitBtnEl = document.querySelector('.submit-btn');
 const hashtagListEl = document.querySelector('.hashtags');
+
+
+const renderFeedbackItem = feedbackItem => {
+  // new feedback item HTML
+  const feedbackItemHTML = `
+      <li class="feedback">
+          <button class="upvote">
+              <i class="fa-solid fa-caret-up upvote__icon"></i>
+              <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+          </button>
+          <section class="feedback__badge">
+              <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+          </section>
+          <div class="feedback__content">
+              <p class="feedback__company">${feedbackItem.company}</p>
+              <p class="feedback__text">${feedbackItem.text}</p>
+          </div>
+          <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+      </li>
+  `;
+
+  // insert new feedback item in list
+  feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+};
