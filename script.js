389b12bd1ba99d9ccbf7d68f07a5d906f0ae17ce
aminetiouk <tiouk.amine@gmail.com>
2025-02-24 22:18:07 +0000

    feat: add global constants and DOM selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+// GLOBAL
+const MAX_CHARS = 150;
+const BASE_API_URL = 'https://bytegrad.com/course-assets/js/1/api';
+
+const textareaEl = document.querySelector('.form_textarea');
+const counterEl = document.querySelector('.counter');
+const formEl = document.querySelector('.form');
+const feedbackListEl = document.querySelector('.feedbacks');
+const submitBtnEl = document.querySelector('.submit-btn');
+const hashtagListEl = document.querySelector('.hashtags');
