7f040818514dc99e36a3fa248dbdb6a9d6d9ff18
aminetiouk <tiouk.amine@gmail.com>
2025-02-24 22:37:25 +0000

    feat: add character counter for textarea

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ const renderFeedbackItem = feedbackItem => {
   // insert new feedback item in list
   feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
 };
+
+// COUNTER COMPONENT
+(() => {
+  const inputHandler = () => {
+    // determine maximum number of characters
+    const maxNrChars = MAX_CHARS;
+
+    // determine number of characters currently typed
+    const nrCharsTyped = textareaEl.value.length;
+
+    // calculate number of characters left (maximum minus currently typed)
+    const charsLeft = maxNrChars - nrCharsTyped;
+
+    // show number of characters left
+    counterEl.textContent = charsLeft;
+  };
+
+  textareaEl.addEventListener('input', inputHandler);
+})();
