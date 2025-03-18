85308780db96d919be2ae966ab5cca1aae8f8554
aminetiouk <tiouk.amine@gmail.com>
2025-03-18 22:56:31 +0000

    feat: initial structure feedbackform

--- src/Components/FeedbackForm.tsx
+++ src/Components/FeedbackForm.tsx
@@ -1,5 +1,16 @@
 export default function FeedbackForm() {
   return (
-    <div>FeedbackForm</div>
+    <form>
+      <textarea id="feedback-textarea" spellCheck={false}></textarea>
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+      <div>
+        <p>{150}</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
   )
 }
