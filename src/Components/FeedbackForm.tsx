2943cbbf9ae3be8b6874d3d88e12f94592d1451c
aminetiouk <tiouk.amine@gmail.com>
2025-03-19 00:14:27 +0000

    feat: implement function for char limit

--- src/Components/FeedbackForm.tsx
+++ src/Components/FeedbackForm.tsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
+const CHARACTER_LIMIT = 150;
+
 export default function FeedbackForm() {
+  const [text, setText] = useState('');
+  const charCount = CHARACTER_LIMIT - text.length;
   return (
     <form>
-      <textarea id="feedback-textarea" spellCheck={false}></textarea>
+      <textarea
+        id="feedback-textarea"
+        spellCheck={false}
+        value={text}
+        onChange={(event) => {
+          const newText = event.target.value;
+          if (newText.length > CHARACTER_LIMIT) {
+            return;
+          }
+          setText(newText)
+        }}
+      ></textarea>
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p>{150}</p>
+        <p>{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
       </div>
     </form>
-  )
+  );
 }
