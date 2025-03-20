63e2789d3cc2d4f22f3e1d33a5f77e29a8037acb
aminetiouk <tiouk.amine@gmail.com>
2025-03-20 01:03:32 +0000

    feat: structure feedback list

--- src/Components/FeedbackList.tsx
+++ src/Components/FeedbackList.tsx
@@ -1,5 +1,26 @@
+import { BiSolidUpArrow } from 'react-icons/bi';
+
 export default function FeedbackList() {
   return (
-    <ol>FeedbackList</ol>
-  )
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <BiSolidUpArrow />
+          <span>593</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>Amine</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
+            qui repellendus error repellat reiciendis enim amet odio mollitia,
+            temporibus iusto.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 }
