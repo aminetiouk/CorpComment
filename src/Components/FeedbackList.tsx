50e1da8eff43e04b16e9106f05b3146e5d27f993
aminetiouk <tiouk.amine@gmail.com>
2025-03-20 01:10:59 +0000

    refactor: implement feedbackItem component

--- src/Components/FeedbackList.tsx
+++ src/Components/FeedbackList.tsx
@@ -1,26 +1,9 @@
-import { BiSolidUpArrow } from 'react-icons/bi';
+import FeedbackItem from './FeedbackItem';
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <BiSolidUpArrow />
-          <span>593</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-        <div>
-          <p>Amine</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
-            qui repellendus error repellat reiciendis enim amet odio mollitia,
-            temporibus iusto.
-          </p>
-        </div>
-        <p>4d</p>
-      </li>
+      <FeedbackItem />
     </ol>
   );
 }
