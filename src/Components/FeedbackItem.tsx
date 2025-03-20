01ffc0b66fb45cc3c857dc648df749354ccff843
aminetiouk <tiouk.amine@gmail.com>
2025-03-20 01:10:16 +0000

    feat: add feedback item component

---
+++ src/Components/FeedbackItem.tsx
@@ -0,0 +1,24 @@
+import { BiSolidUpArrow } from 'react-icons/bi'
+
+export default function FeedbackItem() {
+  return (
+    <li className="feedback">
+        <button>
+          <BiSolidUpArrow />
+          <span>593</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>Amine</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
+            qui repellendus error repellat reiciendis enim amet odio mollitia,
+            temporibus iusto.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+  )
+}
