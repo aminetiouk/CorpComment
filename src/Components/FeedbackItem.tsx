2e79473ab8d17fa0374adc3d5180777a9a44d614
aminetiouk <tiouk.amine@gmail.com>
2025-03-25 18:32:29 +0000

    refactor: implement data from api

--- src/Components/FeedbackItem.tsx
+++ src/Components/FeedbackItem.tsx
@@ -1,24 +1,35 @@
-import { BiSolidUpArrow } from 'react-icons/bi'
+import { BiSolidUpArrow } from 'react-icons/bi';
 
-export default function FeedbackItem() {
+export type Feedback = {
+  id: number;
+  company: string;
+  badgeLetter: string;
+  upvoteCount: number;
+  text: string;
+  daysAgo: number;
+};
+
+type FeedbackItem = {
+  feedbackItem: Feedback;
+};
+
+export default function FeedbackItem({ feedbackItem }: FeedbackItem) {
   return (
     <li className="feedback">
-        <button>
-          <BiSolidUpArrow />
-          <span>593</span>
-        </button>
-        <div>
-          <p>B</p>
-        </div>
-        <div>
-          <p>Amine</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
-            qui repellendus error repellat reiciendis enim amet odio mollitia,
-            temporibus iusto.
-          </p>
-        </div>
-        <p>4d</p>
-      </li>
-  )
+      <button>
+        <BiSolidUpArrow />
+        <span>{feedbackItem.upvoteCount}</span>
+      </button>
+      <div>
+        <p>{feedbackItem.badgeLetter}</p>
+      </div>
+      <div>
+        <p>{feedbackItem.company}</p>
+        <p>
+          {feedbackItem.text}
+        </p>
+      </div>
+      <p>{feedbackItem.daysAgo} d</p>
+    </li>
+  );
 }
