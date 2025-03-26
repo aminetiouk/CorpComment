bd7df7d13c57c0b41f9b910bc00fd1a11efc3282
aminetiouk <tiouk.amine@gmail.com>
2025-03-26 22:40:37 +0000

    feat: props handling

--- src/Components/Container.tsx
+++ src/Components/Container.tsx
@@ -1,11 +1,28 @@
-import FeedbackList from "./FeedbackList";
-import Header from "./Header";
+import { TFeedback } from '../lib/type';
+import FeedbackList from './FeedbackList';
+import Header from './Header';
 
-export default function Container() {
+type ContainerProps = {
+  feedbackItems: TFeedback[];
+  errorMessage: string;
+  isLoading: boolean;
+  handleAddToList: (text: string) => void;
+};
+
+export default function Container({
+  feedbackItems,
+  errorMessage,
+  isLoading,
+  handleAddToList,
+}: ContainerProps) {
   return (
     <div className="container">
-      <Header />
-      <FeedbackList />
+      <Header handleAddToList={handleAddToList} />
+      <FeedbackList
+        feedbackItems={feedbackItems}
+        errorMessage={errorMessage}
+        isLoading={isLoading}
+      />
     </div>
-  )
+  );
 }
