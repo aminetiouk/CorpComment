6dee0f38a7f35169dec3bea15a987e3d226df2ca
aminetiouk <tiouk.amine@gmail.com>
2025-03-29 23:21:19 +0000

    feat: add upvote functionality to feedbackItem

--- src/Components/Feedback/FeedbackItem.tsx
+++ src/Components/Feedback/FeedbackItem.tsx
@@ -7,23 +7,30 @@ type FeedbackItemProps = {
 };
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
-
   const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setUpvoteCount(prev => prev + 1);
+    e.currentTarget.disabled = true;
+    e.stopPropagation();
+  };
 
   return (
-    <li onClick={() => setOpen((prev) => (!prev))} className={`feedback ${open ? "feedback--expand" : ""}`}>
-      <button>
+    <li
+      onClick={() => setOpen((prev) => !prev)}
+      className={`feedback ${open ? 'feedback--expand' : ''}`}
+    >
+      <button onClick={handleUpvote}>
         <BiSolidUpArrow />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
       </div>
       <div>
         <p>{feedbackItem.company}</p>
-        <p>
-          {feedbackItem.text}
-        </p>
+        <p>{feedbackItem.text}</p>
       </div>
       <p>{feedbackItem.daysAgo} d</p>
     </li>
