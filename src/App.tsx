46318469d7631e4020bd9ca7080a28057f0cf9d3
aminetiouk <tiouk.amine@gmail.com>
2025-04-01 23:13:41 +0000

    Wrap application with FeedbackContextItemsProvider for global state m anagement

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ import { TFeedback } from './lib/type';
 import HashtagItem from './components/Hashtag/HashtagItem';
 
 function App() {
-  const [feedbackItems, setFeedbackItem] = useState<TFeedback[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
   const [selectCompany, setSelectCompany] = useState('');
 
   const filterFeedbackItem = useMemo(
@@ -21,73 +18,9 @@ function App() {
     [feedbackItems, selectCompany]
   );
 
-  const companyList = useMemo(
-    () =>
-      feedbackItems
-        .map(name => name.company)
-        .filter((company, index, array) => {
-          return array.indexOf(company) === index;
-        }),
-    [feedbackItems]
-  );
-
-  const handleAddToList = async (text: string) => {
-    const companyName = text
-      .split(' ')
-      .find(word => word.includes('#'))!
-      .substring(1);
-
-    const newItem: TFeedback = {
-      id: new Date().getTime(),
-      company: companyName,
-      badgeLetter: companyName.substring(0, 1).toUpperCase(),
-      daysAgo: 0,
-      upvoteCount: 0,
-      text: text
-    };
-    setFeedbackItem([...feedbackItems, newItem]);
-
-    await fetch(
-      'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks',
-      {
-        method: 'POST',
-        body: JSON.stringify(newItem),
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-  };
-
   const handleSelectCompany = (company: string) => {
     setSelectCompany(company);
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(
-          'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
-        );
-
-        if (!response.ok) {
-          throw new Error();
-        }
-
-        const data = await response.json();
-        setFeedbackItem(data.feedbacks);
-      } catch (error) {
-        console.error('Error fetching feedbacks:', error);
-        setErrorMessage(`⚠️ Failed to load data`);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
     <div className="app">
