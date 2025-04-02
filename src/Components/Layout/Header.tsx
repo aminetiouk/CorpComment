328cfe2dd9ec3f6eb3b5e035e06757acf6c0422b
aminetiouk <tiouk.amine@gmail.com>
2025-04-02 23:23:35 +0000

    Refactor Header component for context integration

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,13 +1,11 @@
+import { useFeedbackItemsContext } from '../../lib/hooks';
 import FeedbackForm from '../Feedback/FeedbackForm';
 import Logo from '../Logo';
 import PageHeading from '../PageHeading';
 import Pattern from '../Pattern';
 
-type HeaderProps = {
-  handleAddToList: (text: string) => void;
-};
-
-export default function Header({ handleAddToList }: HeaderProps) {
+export default function Header() {
+  const { handleAddToList } = useFeedbackItemsContext();
   return (
     <header>
       <Pattern />
