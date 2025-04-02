0fc031a244ca3e573d5286b49c23056200cb5f9b
aminetiouk <tiouk.amine@gmail.com>
2025-04-02 23:22:53 +0000

    Refactor App.tsx to use Context API

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,17 @@
-import { useEffect, useMemo, useState } from 'react';
 import Container from './components/Layout/Container';
 import Footer from './components/Layout/Footer';
 import HashtagList from './components/Hashtag/HashtagList';
-import { TFeedback } from './lib/type';
-import HashtagItem from './components/Hashtag/HashtagItem';
+import FeedbackContextItemsProvider from './components/contexts/FeedbackContextItemsProvider';
 
 function App() {
-  const [selectCompany, setSelectCompany] = useState('');
-
-  const filterFeedbackItem = useMemo(
-    () =>
-      selectCompany
-        ? feedbackItems.filter(
-            feedbackItem => feedbackItem.company === selectCompany
-          )
-        : feedbackItems,
-    [feedbackItems, selectCompany]
-  );
-
-  const handleSelectCompany = (company: string) => {
-    setSelectCompany(company);
-  };
-
   return (
     <div className="app">
       <Footer />
 
-      <Container
-        feedbackItems={filterFeedbackItem}
-        isLoading={isLoading}
-        errorMessage={errorMessage}
-        handleAddToList={handleAddToList}
-      />
-
-      <HashtagList>
-        {companyList.map((company: string) => (
-          <HashtagItem
-            company={company}
-            onSelectCompany={handleSelectCompany}
-          />
-        ))}
-      </HashtagList>
+      <FeedbackContextItemsProvider>
+        <Container />
+        <HashtagList />
+      </FeedbackContextItemsProvider>
     </div>
   );
 }
