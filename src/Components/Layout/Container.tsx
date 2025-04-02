aa97ecfc02966cc001b012b8eb43fdcec77192cc
aminetiouk <tiouk.amine@gmail.com>
2025-04-02 23:23:21 +0000

    Update Container component to work with Context API

--- src/Components/Layout/Container.tsx
+++ src/Components/Layout/Container.tsx
@@ -1,29 +1,11 @@
-import { TFeedback } from '../../lib/type';
 import FeedbackList from '../Feedback/FeedbackList';
 import Header from './Header';
 
-
-type ContainerProps = {
-  feedbackItems: TFeedback[];
-  errorMessage: string;
-  isLoading: boolean;
-  handleAddToList: (text: string) => void;
-};
-
-export default function Container({
-  feedbackItems,
-  errorMessage,
-  isLoading,
-  handleAddToList
-}: ContainerProps) {
+export default function Container() {
   return (
     <div className="container">
-      <Header handleAddToList={handleAddToList} />
-      <FeedbackList
-        feedbackItems={feedbackItems}
-        errorMessage={errorMessage}
-        isLoading={isLoading}
-      />
+      <Header />
+      <FeedbackList />
     </div>
   );
 }
