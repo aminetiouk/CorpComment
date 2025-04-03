7852b71b0f268ce8e0a3fd87913c23f8c6e7b1d5
aminetiouk <tiouk.amine@gmail.com>
2025-04-03 22:21:59 +0000

    extract feedback fetching logic into useFeedbackItem hook

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,6 @@
-import { useContext } from "react";
-import { FeedbackItemsContext } from "../components/contexts/FeedbackContextItemsProvider";
+import { useContext, useEffect, useState } from 'react';
+import { FeedbackItemsContext } from '../components/contexts/FeedbackContextItemsProvider';
+import { TFeedback } from './type';
 
 export function useFeedbackItemsContext() {
   const context = useContext(FeedbackItemsContext);
@@ -9,4 +10,42 @@ export function useFeedbackItemsContext() {
     );
   }
   return context;
+}
+
+export function useFeedbackItem() {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedback[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          'https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks'
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        console.error('Error fetching feedbacks:', error);
+        setErrorMessage(`⚠️ Failed to load data`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return {
+    isLoading,
+    errorMessage,
+    feedbackItems,
+    setFeedbackItems
+  }
 }
