4b354351c7f94a35b587e7a08c7210cd5aeff4f3
aminetiouk <tiouk.amine@gmail.com>
2025-04-04 23:36:42 +0000

    refactor(HashtagList): update to use global store

--- src/Components/Hashtag/HashtagList.tsx
+++ src/Components/Hashtag/HashtagList.tsx
@@ -1,12 +1,13 @@
-import { useFeedbackItemsContext } from '../../lib/hooks';
+import { useFeedbackItemsStore } from '../../stores/feedbackItemsStore';
 import HashtagItem from './HashtagItem';
 
 export default function HashtagList() {
-  const { companyList, handleSelectCompany } = useFeedbackItemsContext();
+  const companyList = useFeedbackItemsStore(state => state.getCompanyList());
+  const selectCompany = useFeedbackItemsStore(state => state.selectCompany());
   return (
     <ul className="hashtags">
       {companyList.map((company: string) => (
-        <HashtagItem company={company} onSelectCompany={handleSelectCompany} />
+        <HashtagItem company={company} onSelectCompany={selectCompany} />
       ))}
     </ul>
   );
