27ee213e8b3729b8ec9cc408f25bde6a22671f8f
aminetiouk <tiouk.amine@gmail.com>
2025-04-04 23:36:42 +0000

    refactor(App): integrate Zustand state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import Container from './components/Layout/Container';
 import Footer from './components/Layout/Footer';
 import HashtagList from './components/Hashtag/HashtagList';
-import FeedbackContextItemsProvider from './components/contexts/FeedbackContextItemsProvider';
+import { useFeedbackItemsStore } from './stores/feedbackItemsStore';
+import { useEffect } from 'react';
 
 function App() {
+  const fetchFeedbackItems = useFeedbackItemsStore(
+    state => state.fetchFeedbackItems
+  );
+
+  useEffect(() => {
+    fetchFeedbackItems();
+  }, [fetchFeedbackItems]);
+
   return (
     <div className="app">
       <Footer />
 
-      <FeedbackContextItemsProvider>
-        <Container />
-        <HashtagList />
-      </FeedbackContextItemsProvider>
+      <Container />
+
+      <HashtagList />
     </div>
   );
 }
