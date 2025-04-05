3f3d23bccb0d17b237cda697c239991de28edf88
aminetiouk <tiouk.amine@gmail.com>
2025-04-05 23:18:28 +0000

    refactor: use arrow function with parentheses for clarity in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 function App() {
   const fetchFeedbackItems = useFeedbackItemsStore(
-    state => state.fetchFeedbackItems
+    (state) => state.fetchFeedbackItems
   );
 
   useEffect(() => {
